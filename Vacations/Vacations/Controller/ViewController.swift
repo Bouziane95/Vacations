844ca51d2dd0b5d873e781a167a3e76ae9d2b8d3
Bouziane <bey.bouziane@live.fr>
2019-07-23 18:45:41 +0200

    MVC + first commit

---
+++ Vacations/Vacations/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vacations
+//
+//  Created by Bouziane Bey on 23/07/2019.
+//  Copyright © 2019 Bouziane Bey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
