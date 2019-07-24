3db7d75d8d11647cf28c52d0d5c02d01b58ac932
Bouziane <bey.bouziane@live.fr>
2019-07-24 16:16:52 +0200

    Table View cell logic

---
+++ Vacations/Vacations/Controller/MapVC.swift
@@ -0,0 +1,30 @@
+//
+//  MapVC.swift
+//  Vacations
+//
+//  Created by Bouziane Bey on 24/07/2019.
+//  Copyright © 2019 Bouziane Bey. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
